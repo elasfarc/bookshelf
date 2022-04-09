9d2251124e7842fac22bee59e60f98da8ebc072a
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-10 00:07:08 +0200

    Add login / register modals

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
+import React from "react";
 import "./App.css";
 import { Logo } from "./components/logo";
+import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog";
+import "@reach/dialog/styles.css";
+const R = require("ramda");
+
 function App() {
+  const [showModal, setShowModal] = React.useState(false);
+  const showLoginModal = showModal === "login";
+  const showRegisterModal = showModal === "register";
+  const closeModal = R.compose(setShowModal, R.F);
+
   return (
     <>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert("login is clicked")}>login</button>
+        <button onClick={R.compose(setShowModal, R.always("login"))}>
+          login
+        </button>
       </div>
       <div>
-        <button onClick={() => alert("register is clicked")}>register</button>
+        <button onClick={R.compose(setShowModal, R.always("register"))}>
+          register
+        </button>
       </div>
+      <Dialog
+        aria-label="Login form"
+        isOpen={showLoginModal}
+        onDismiss={closeModal}
+      >
+        <button className="close-button" onClick={closeModal}>
+          <span aria-hidden>×</span>
+        </button>
+        Login
+      </Dialog>
+      <Dialog
+        aria-label="Register form"
+        isOpen={showRegisterModal}
+        onDismiss={closeModal}
+      >
+        <button className="close-button" onClick={closeModal}>
+          <span aria-hidden>×</span>
+        </button>
+        Register
+      </Dialog>
     </>
   );
 }
