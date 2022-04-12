3f612abda89ad6f8391cbf8d858009a0bd91c015
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-12 06:25:38 +0200

    Add colors and media queries files

--- src/components/lib.js
+++ src/components/lib.js
@@ -1,17 +1,19 @@
 import styled from "@emotion/styled/macro";
 import { Dialog as ReachDialog } from "@reach/dialog";
+import * as colors from "../styles/colors";
+import * as mq from "../styles/media-queries";
 
 const Input = styled.input({
   width: "100%",
   borderRadius: "3px",
-  border: "1px solid #f1f1f4",
-  background: "#f1f2f7",
+  border: `1px solid ${colors.gray10}`,
+  background: colors.gray,
   padding: "8px 12px",
 });
 
 const Button = styled.button(({ variant }) => ({
-  color: variant === "primary" ? "#fff" : "#434449",
-  backgroundColor: variant === "primary" ? "#3F51B5" : "#F1F2F7",
+  color: variant === "primary" ? colors.base : colors.text,
+  backgroundColor: variant === "primary" ? colors.indigo : colors.gray,
   borderRadius: "3px",
   padding: "10px 15px",
   border: "none",
@@ -24,7 +26,7 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: "3.5em",
   boxShadow: "0 10px 30px -5px rgba(0, 0, 0, 0.2)",
   margin: "20vh auto",
-  "@media (max-width: 991px)": {
+  [mq.small]: {
     width: "100%",
     margin: "10vh auto",
   },
