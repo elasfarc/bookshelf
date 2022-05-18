6490d9a7edb78ec719894a24295b0eefa27640bc
Mohammed Al-Asfar <63879921+elasfarc@users.noreply.github.com>
2022-05-18 20:48:53 +0200

    Add SortBox

--- src/components/list-item-list.js
+++ src/components/list-item-list.js
@@ -1,7 +1,10 @@
 /** @jsxImportSource @emotion/react */
+import React from "react";
 import { useUserList } from "../util/react-query/user-list";
+import { sortObjBy } from "../util/sort-user-list";
 import BookList from "../components/book-list";
 import { Spinner } from "./lib";
+import SortBox from "./sort-box";
 
 function ListItemList({
   filterListItems,
@@ -10,7 +13,19 @@ function ListItemList({
   sortOptions,
   defaultSort,
 }) {
+  const [sort, setSort] = React.useState({
+    sortBy: sortOptions.sortBy.TIME_ADDED,
+    order: sortOptions.order.ASC,
+  });
+
   const { userList, isIdle, isLoading } = useUserList();
+
+  const handleSortChange = (e) => {
+    setSort((prevOptions) => ({
+      ...prevOptions,
+      [e.target.name]: sortOptions[e.target.name][e.target.value],
+    }));
+  };
 
   if (isIdle || isLoading)
     return (
@@ -32,6 +47,13 @@ function ListItemList({
       <div css={{ marginTop: "1em", fontSize: "1.2em" }}>{onNoListItems}</div>
     );
   const filteredListItems = userListItems.filter(filterListItems);
+
+  const sortedUserList = sortObjBy({
+    propPath: sort.sortBy,
+    order: sort.order,
+    data: filteredListItems,
+  });
+
   if (filteredListItems.length === 0)
     return (
       <div css={{ marginTop: "1em", fontSize: "1.2em" }}>
@@ -40,12 +62,22 @@ function ListItemList({
     );
 
   return (
-    <BookList
-      books={filteredListItems.map(({ itemId, itemData }) => ({
-        id: itemId,
-        volumeInfo: itemData,
-      }))}
-    />
+    <div css={{ display: "flex", flexDirection: "column" }}>
+      {Object.entries(sortOptions).map(([sortFor, sortOptions]) => (
+        <SortBox
+          groupFor={sortFor}
+          options={sortOptions}
+          handleChange={handleSortChange}
+          defaultSort={defaultSort}
+        />
+      ))}
+      <BookList
+        books={sortedUserList.map(({ itemId, itemData }) => ({
+          id: itemId,
+          volumeInfo: itemData,
+        }))}
+      />
+    </div>
   );
 }
 
